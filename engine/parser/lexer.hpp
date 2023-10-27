4b5b73ef9320117319004267bab5ee93df5520c8
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2023-10-27 14:30:20 -0600

    begin filling in lexer implementation

--- engine/parser/lexer.hpp
+++ engine/parser/lexer.hpp
@@ -1,14 +1,21 @@
 #ifndef PHI_MATH_ENGINE_PARSER_LEXER_HPP
 #define PHI_MATH_ENGINE_PARSER_LEXER_HPP
 
+#include "../utility/buffered_input_wrapper.hpp"
+#include "../datatype/token.hpp"
 #include <iterator>
+#include <cctype>
 
 namespace phimath {
 
-template <std::input_iterator stream> requires std::same_as<std::iter_value_t<stream>, char>
+template <std::input_iterator input_type, typename sentinel_type = input_type> requires std::same_as<std::iter_value_t<input_type>, char> && std::sentinel_for<sentinel_type, input_type>
 class Lexer {
     private:
+        buffered_input_wrapper stream;
+        const sentinel_type& end;
+
     public:
+        Lexer(input_type&& stream, const sentinel_type&& end) : stream(buffered_input_wrapper(std::move(stream))), end(end) {}
 };
 
 };
