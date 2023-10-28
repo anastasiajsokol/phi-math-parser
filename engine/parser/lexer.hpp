12bd903c5f955d7315c70f8498a3b024d98ef111
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2023-10-27 18:29:45 -0600

    begin implementing Lexer::token_iterator

--- engine/parser/lexer.hpp
+++ engine/parser/lexer.hpp
@@ -11,11 +11,43 @@ namespace phimath {
 template <std::input_iterator input_type, typename sentinel_type = input_type> requires std::same_as<std::iter_value_t<input_type>, char> && std::sentinel_for<sentinel_type, input_type>
 class Lexer {
     private:
-        buffered_input_wrapper<input_type, sentinel_type> stream;
-        const sentinel_type& end;
+        class token_iterator {
+            private:
+                buffered_input_wrapper<input_type, sentinel_type> stream;
+                const sentinel_type& end;
+                Token buffer;
+
+                void load_next_token(){
+                    
+                }
+
+            public:
+                token_iterator(input_type&& stream, const sentinel_type& end) : stream(std::move(stream)), end(end) {}
+
+                token_iterator& operator ++(){
+                    load_next_token();
+                    return *this;
+                }
+
+                const Token& operator *() const {
+                    return buffer;
+                }
+                
+                bool operator ==(const Token& token) const {
+                    return buffer.type == token.type;
+                }
+        } iterator;
 
     public:
-        Lexer(input_type&& stream, const sentinel_type&& end) : stream(std::move(stream)), end(end) {}
+        Lexer(input_type&& stream, const sentinel_type& end) : iterator(std::move(stream), end(end)) {}
+
+        token_iterator& begin(){
+            return iterator;
+        }
+
+        Token end(){
+            return Token{-1, -1, "EOI", TokenType::eoi};
+        }
 };
 
 };
